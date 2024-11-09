f634d4ded91314c998ac6bab04916acb7aab265a
maaxon <wmaxwas@gmail.com>
2024-11-09 16:01:19 +0300

    fix(get-base-url): add host var

--- src/utils/get-base-url/get-base-url.ts
+++ src/utils/get-base-url/get-base-url.ts
@@ -1,6 +1,6 @@
 export const getBaseUrl = (): string => {
   if (process.env.NEXT_PUBLIC_VERCEL_URL) {
-    return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
+    return `https://https://client-blog-tan.vercel.app`;
   }
 
   return "http://localhost:3000";
