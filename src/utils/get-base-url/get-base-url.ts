b3c916aef438fb0d5fa8fbf3106f33724899a5cf
maaxon <wmaxwas@gmail.com>
2024-11-09 15:04:59 +0300

    fix(get-base-url): add env for production

--- src/utils/get-base-url/get-base-url.ts
+++ src/utils/get-base-url/get-base-url.ts
@@ -1,6 +1,6 @@
 export const getBaseUrl = (): string => {
-  if (process.env.VERCEL_URL) {
-    return `https://${process.env.VERCEL_URL}`;
+  if (process.env.NEXT_PUBLIC_VERCEL_URL) {
+    return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
   }
 
   return "http://localhost:3000";
